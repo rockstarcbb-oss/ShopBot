0c5a7aa40baf8bb228258ae6635a7fa082bd3e16
rockstarcbb-oss <rockstarcbb@gmail.com>
2026-09-02 15:19:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -78,6 +78,9 @@ async def _startup() -> None:
     if config.CRYPTO_FORWARDING_MODE:
         for cryptocurrency in Cryptocurrency:
             forwarding_address = cryptocurrency.get_forwarding_address()
+            # Jei adresas tuščias arba nerastas, tiesiog praleidžiame validaciją ir nebeišjungiame boto
+            if not forwarding_address or forwarding_address in ["None", "false", ""]:
+                continue
             is_addr_valid = WalletService.validate_withdrawal_address(forwarding_address, cryptocurrency)
             if is_addr_valid is False:
                 logging.error(
